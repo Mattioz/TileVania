fe5af2a807fa66d1bd4cec560742af801fa6fb48
Mattioz <61207452+Mattioz@users.noreply.github.com>
2023-08-26 13:13:37 -0300

    Add Horizontal Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,30 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 5f;
+
     Vector2 moveInput;
+    Rigidbody2D rb2d;
     
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     
     void Update()
-    {
-        
+    {   
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    void Run()
+    {   
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb2d.velocity.y);
+        rb2d.velocity = playerVelocity;
     }
 }
