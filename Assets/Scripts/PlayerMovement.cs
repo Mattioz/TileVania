e71c9be8521c1fd3997f0968b22a134fbae5b41a
Mattioz <61207452+Mattioz@users.noreply.github.com>
2023-08-26 13:33:21 -0300

    Flip Sprite

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {   
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
@@ -30,5 +31,15 @@ public class PlayerMovement : MonoBehaviour
     {   
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb2d.velocity.y);
         rb2d.velocity = playerVelocity;
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb2d.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(rb2d.velocity.x), 1f);
+        }
     }
 }
