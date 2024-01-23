0233cdb09c35a0a3b99c2f9406cc10f60b79af58
Mattioz <61207452+Mattioz@users.noreply.github.com>
2024-01-23 12:49:29 -0300

    Add LevelExit Script

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float levelLoadDelay = 1f;
+    
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        StartCoroutine(LoadNextLevel());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadNextLevel()
     {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
         
+        SceneManager.LoadScene(nextSceneIndex);
     }
+
 }
